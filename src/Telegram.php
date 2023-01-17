45a357033bdb6df00ba07c47a615e06951d0d1bd
wildhandb <wildhan.db@gmail.com>
2023-01-17 21:10:15 +0700

    Optimize ✅

--- src/Telegram.php
+++ src/Telegram.php
@@ -2,18 +2,10 @@
 
 namespace Debva\PhpTelegram;
 
-use Debva\PhpTelegram\Concerns\{
-    AvailableMethods,
-    AvailableTypes,
-    GettingUpdates,
-    UpdatingMessages,
-    Utilities
-};
+use Debva\PhpTelegram\Exceptions\TelegramException;
 
 class Telegram
 {
-    use AvailableMethods, AvailableTypes, GettingUpdates, UpdatingMessages, Utilities;
-
     public const PARSE_MODE_HTML = 'HTML';
 
     public const PARSE_MODE_MARKDOWN = 'MarkdownV2';
@@ -22,10 +14,61 @@ class Telegram
 
     protected $endpoint;
 
-    protected $content;
+    protected $parameter;
 
     public function __construct($token)
     {
         $this->url = "{$this->url}{$token}";
+    }
+
+    public function __call($endpoint, $parameter)
+    {
+        try {
+            if (in_array($endpoint, $this->endpointList())) {
+                $this->endpoint = $endpoint;
+                $this->parameter = end($parameter);
+                return $this->send();
+            }
+
+            throw new TelegramException('Endpoint not found');
+        } catch (TelegramException $e) {
+            echo $e->getMessage();
+            return $e->getMessage();
+        }
+    }
+
+    public function inlineKeyboardButton($keyboard)
+    {
+        return json_encode(["inline_keyboard" => [$keyboard]]);
+    }
+
+    public function listenWebhook(\Closure $closure)
+    {
+        $listen = json_decode(file_get_contents('php://input'), true);
+        if (!is_null($listen) and isset($listen['callback_query'])) {
+            $closure = $closure->bindTo($this, __CLASS__);
+            $closure($listen['callback_query'], $listen['update_id']);
+            exit;
+        }
+    }
+
+    protected function send()
+    {
+        return json_decode(file_get_contents("{$this->url}/{$this->endpoint}", false, stream_context_create([
+            'http' => [
+                'header'  => "Content-type: application/x-www-form-urlencoded",
+                'method'  => 'POST',
+                'content' => http_build_query($this->parameter)
+            ]
+        ])));
+    }
+
+    protected function endpointList()
+    {
+        return [
+            'sendMessage',
+            'editMessageText',
+            'setWebhook',
+        ];
     }
 }
